b40d664430bec1d0edb5f300410acb0af8554c81
Mathis Da Silva <mathis.da-silva@epitech.eu>
2023-12-03 19:02:27 +0100

    some changes

--- scenes/second_character.gd
+++ scenes/second_character.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -900.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
 @onready var sprite = $Sprite2D
 @onready var anim = get_node("AnimationPlayer")
