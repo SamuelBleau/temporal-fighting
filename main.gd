46b2a81fcc13199ad75d69f9938aa7ec6ac5ad95
Samuel Bleau <samuel.bleau@epitech.eu>
2023-12-05 17:51:50 +0100

    oui

--- main.gd
+++ main.gd
@@ -1,12 +1,20 @@
 extends Node
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_released("quit"):
 		get_tree().change_scene_to_file("res://init/menu.tscn")
+	if ($Timer.time_left < 10):
+		$timer.text = "0" + str(int($Timer.time_left))
+	if ($Timer.time_left > 10):
+		$timer.text = str(int($Timer.time_left))
+
+
+func _on_timer_timeout():
+	$Timer.stop()
+	$timer.text = "0"
